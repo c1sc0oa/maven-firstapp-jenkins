b5d4bd031b91f883c78766c7b5e13c453fb4c739
c1sc0oa <80284760+c1sc0oa@users.noreply.github.com>
2023-10-20 12:33:17 -0600

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,13 +11,13 @@ set +x
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME="$(mvn help:evaluate -Dexpression=project.name | grep '^[^\[]' | tr -d '\r')"
+NAME="$(mvn help:evaluate -Dexpression=project.name | grep '^[^\[]' | tr -d '\r' | sed -e 's/^[ \t]*//')"
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION="$(mvn help:evaluate -Dexpression=project.version | grep '^[^\[]' | tr -d '\r')"
+VERSION="$(mvn help:evaluate -Dexpression=project.version | grep '^[^\[]' | tr -d '\r' | sed -e 's/^[ \t]*//')"
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
